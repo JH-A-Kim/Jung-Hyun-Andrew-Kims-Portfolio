d06265a88acb3a29dacb63bc5e0755de2dd524f4
akim0 <kimisawesome72@yahoo.com>
2024-07-01 21:37:03 -0700

    fixed the animation

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,41 +1,48 @@
 "use client"
 
-import React from "react"
-import {motion} from "framer-motion"
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion"
-import { SparklesIcon } from "@heroicons/react/24/solid"
-import Image from "next/image"
+import React from "react";
+import {motion} from "framer-motion";
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
 export const HeroContent = () => {
   return (
     <motion.div
-    initial="Hidden"
-    animate="Visible"
-    className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
         <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
           <motion.div 
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px [4px] border border-[#7042f88b] opacitty-[0.9]">
-            <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/>
-            <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
-          </motion.div>
-
-          <motion.div 
-          variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+            variants={slideInFromTop}
+            className="Welcome-box py-[8px] px-4 border border-[#7042f88b] opacitty-[0.9]"
           >
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">The best</span>
-            Project Experience
+            <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5"/>
+            <h1 className="Welcome-text text-[13px]">Fullstack Developer Portfolio</h1>
           </motion.div>
 
-          <motion.p
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            project exprience
+          </span>
+        </motion.div>
+
+        <motion.p
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
           >
             I'm a Full Stack Software Engineer with Experience in Website, Mobile, and Software development. Check out my projects and skills.
 
-          </motion.p>
+        </motion.p>
           <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
@@ -55,6 +62,6 @@ export const HeroContent = () => {
           />
         </motion.div>
     </motion.div>
-  )
+  );
 }
 export default HeroContent 
