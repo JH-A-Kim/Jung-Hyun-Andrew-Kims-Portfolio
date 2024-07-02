017e6021fc98135f1574d3c1fc3a437ba3c9c4f2
akim0 <kimisawesome72@yahoo.com>
2024-07-01 22:11:28 -0700

    created spinning star background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import StarsCanvas from "../components/main/StarBackground";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={' ${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden'}>
+        <StarsCanvas/>
         {children}</body>
     </html>
   );
