a615715c53aac530ed35146fdc9294c341c0269f
akim0 <kimisawesome72@yahoo.com>
2024-07-01 21:41:25 -0700

    adjusted component

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -16,7 +16,7 @@ export const HeroContent = () => {
         <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
           <motion.div 
             variants={slideInFromTop}
-            className="Welcome-box py-[8px] px-4 border border-[#7042f88b] opacitty-[0.9]"
+            className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacitty-[0.9]"
           >
             <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5"/>
             <h1 className="Welcome-text text-[13px]">Fullstack Developer Portfolio</h1>
