f65c3fbb68afb61f749e409f31f68ba076017111
akim0 <kimisawesome72@yahoo.com>
2024-07-02 20:47:03 -0700

    started Skills.tsx

---
+++ components/main/Skills.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'
+    style={{transform: "scale(0.9)"}}
+    >
+        <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Skills
