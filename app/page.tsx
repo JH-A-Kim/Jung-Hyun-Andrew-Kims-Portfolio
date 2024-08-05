882903aba8ee3f03f87d5b2d9583fb74d17fc925
akim0 <kimisawesome72@yahoo.com>
2024-08-04 18:49:44 -0700

    encryption component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Encryption from "@/components/main/Encryption";
 import Hero from "@/components/main/Hero";
+import Skills from "@/components/main/Skills";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className='h-full w-full'>
       <div className='flex flex-col gap-20'>
-        <Hero/>      
+        <Hero/>   
+        <Skills/> 
+        <Encryption/>  
         </div>
     </main>
   );
