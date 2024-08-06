726946a40f15ee309ba69acef1592a2f0a723628
akim0 <kimisawesome72@yahoo.com>
2024-08-05 21:57:18 -0700

    fix png name

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
         <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>My Projects</h1>
         <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
             <ProjectCard src='/NextWebsite.png' title='Project 1' description='This is a description for project 1' />
-            <ProjectCard src='/cardImage.png' title='Project 2' description='This is a description for project 2' />
+            <ProjectCard src='/CardImage.png' title='Project 2' description='This is a description for project 2' />
             <ProjectCard src='/SpaceWebsite.png' title='Project 3' description='https://github.com/' />
         </div>
     </div>
